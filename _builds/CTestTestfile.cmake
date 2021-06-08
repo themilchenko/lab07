0525eb9dac498e1550b957b1dbc54369d5b12a9f
themilchenko <ivan.milchenko.92@mail.ru>
2021-06-08 14:10:52 +0300

    Complete lab07

---
+++ _builds/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/milchenko/themilchenko/workspace/projects/lab07
+# Build directory: /home/milchenko/themilchenko/workspace/projects/lab07/_builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/milchenko/themilchenko/workspace/projects/lab07/_builds/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/milchenko/themilchenko/workspace/projects/lab07/CMakeLists.txt;63;add_test;/home/milchenko/themilchenko/workspace/projects/lab07/CMakeLists.txt;0;")
